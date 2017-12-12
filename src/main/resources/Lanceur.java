35c5a6b54ee40745bf4190c168c4f003ab815ef9
Nina PETARD <nina.petard@gmail.com>
2017-12-12 11:15:03 +0100

    Added addition

--- src/main/resources/Lanceur.java
+++ src/main/resources/Lanceur.java
@@ -15,6 +15,8 @@ public class Lanceur {
      */
     public static void main(String[] args) {
                 System.out.println("Coucou le monde"); 
+                
+                int a = 2+3;
    }
     
 }
