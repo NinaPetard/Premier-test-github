15c61144722732e1b3fd7f35f4d6ae6858e435e2
Nina PETARD <nina.petard@gmail.com>
2017-12-12 11:11:58 +0100

    Created class Lanceur

--- src/main/resources/Lanceur.java
+++ src/main/resources/Lanceur.java
@@ -3,19 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tpnina.test_git;
 
 /**
  *
  * @author adminl
  */
-public class test {
+public class Lanceur {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Coucou");
-    }
+   }
     
 }
